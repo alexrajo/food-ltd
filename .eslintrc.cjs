d9cb068c3a250d88667f3e9ceabc11e06a4cbb99
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-09-27 13:29:52 +0200

    [#1] feat: added extensions to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,15 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-}
+};
