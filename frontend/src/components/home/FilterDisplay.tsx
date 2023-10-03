544a867e42fe0a62dda3cde92676df7a58373f83
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-10-03 15:03:51 +0200

    [#13] feat: added useFilter to FilterDisplay

--- frontend/src/components/home/FilterDisplay.tsx
+++ frontend/src/components/home/FilterDisplay.tsx
@@ -1,10 +1,17 @@
+import { useFilter } from 'hooks';
 import cross from 'src/assets/cross.svg';
 
 export default function FilterDisplay({ name }: { name: string }) {
-    return (
-      <div className="flex items-center gap-2 p-2 border border-black rounded-lg">
-        <div>{name}</div>
-        <img alt="cross" className="w-4 h-4" src={cross} />
-      </div>
-    );
-  }
+  const { onClickRemoveFilter } = useFilter();
+
+  const onClick = () => {
+    onClickRemoveFilter(name);
+  };
+
+  return (
+    <div className='flex items-center gap-2 p-2 border border-black rounded-lg'>
+      <div>{name}</div>
+      <img onClick={onClick} alt='cross' className='w-4 h-4' src={cross} />
+    </div>
+  );
+}
