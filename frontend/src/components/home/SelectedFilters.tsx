4c235e6ddc1d7a93081737627ce52b7ab8c3ff34
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-10-04 09:12:12 +0200

    [#13] feat: removed mockfilters and used redux state instead

--- frontend/src/components/home/SelectedFilters.tsx
+++ frontend/src/components/home/SelectedFilters.tsx
@@ -1,14 +1,16 @@
-import FilterDisplay from "./FilterDisplay";
+import FilterDisplay from './FilterDisplay';
 
 export default function SelectedFilters() {
-    const mockFilters = ['Sukker', 'ost', 'Under 20 minutes'];
-  
-    return (
-      <div className="flex items-center gap-2 flex-wrap">
-        {mockFilters.map((f) => (
-          <FilterDisplay key={f} name={f} />
-        ))}
-        <div className="font-thin text-sm underline">Clear</div>
+  const { filters, onClickResetFilters } = useFilter();
+
+  return (
+    <div className='flex items-center gap-2 flex-wrap'>
+      {filters.map((f) => (
+        <FilterDisplay key={f} name={f} />
+      ))}
+      <div onClick={onClickResetFilters} className='font-thin text-sm underline'>
+        Clear
       </div>
-    );
-  }
+    </div>
+  );
+}
