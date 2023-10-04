6b294c9199bb554bb48014400d5f8c0d0abb8b25
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-10-04 09:17:49 +0200

    [#13] fix: removed .tsx hook

--- frontend/src/hook/useAppRedux.tsx
+++
@@ -1,8 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from 'src/redux/store';
-
-type DispatchFunc = () => AppDispatch;
-/** USE THIS FOR TYPESCRIPT INSTEAD OF REGULAR USEDISPATCH */
-export const useAppDispatch: DispatchFunc = useDispatch;
-/** USE THIS FOR TYPESCRIPT INSTEAD OF REGULAR USESELECTOR */
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
