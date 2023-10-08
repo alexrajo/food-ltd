bac56f0af61eee621101ba811b9559f932855bac
Alexander Rathke Johansen <alexarj@stud.ntnu.no>
2023-10-08 17:00:08 +0200

    [#30] fix: Fix filter selection bug
    Fix bug where filters are added two at a time if the checkbox is clicked.

--- frontend/src/components/home/SelectedFilters.tsx
+++ frontend/src/components/home/SelectedFilters.tsx
@@ -4,8 +4,10 @@ import FilterDisplay from './FilterDisplay';
 export default function SelectedFilters() {
   const { includedFilters, excludedFilters, resetFilters } = useFilter();
 
+  console.log(includedFilters, excludedFilters);
+
   return (
-    <div className="flex items-center gap-2 flex-wrap">
+    <div className='flex items-center gap-2 flex-wrap'>
       {includedFilters.map((f) => (
         <FilterDisplay key={f} name={f} type='include' />
       ))}
@@ -13,7 +15,7 @@ export default function SelectedFilters() {
         <FilterDisplay key={f} name={f} type='exclude' />
       ))}
       {(includedFilters.length > 0 || excludedFilters.length > 0) && (
-        <button type='button' onClick={resetFilters} className="font-thin text-sm underline">
+        <button type='button' onClick={resetFilters} className='font-thin text-sm underline'>
           Clear
         </button>
       )}
