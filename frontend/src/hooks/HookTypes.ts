309ad5ae753075f6b041954e35db68d126803bb7
Sondre Pedersen <sondrp@stud.ntnu.no>
2023-10-08 16:19:41 +0200

    [#40] fix: return type typo

--- frontend/src/hooks/HookTypes.ts
+++ frontend/src/hooks/HookTypes.ts
@@ -286,7 +286,7 @@ export type useSearchReturnType = {
    * }
    *
    */
-  paginateToPage: (page: number) => void;
+  paginateTo: (page: number) => void;
   /**
    * Refetch the search results
    * @returns {void}
