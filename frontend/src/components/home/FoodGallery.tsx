dd60b0079c982e58a37e774eaf0d9164d3fe6b2d
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-10-26 15:58:00 +0200

    [#54] feat: foodgallery now shows skeleton when loading and animation when no items are found

--- frontend/src/components/home/FoodGallery.tsx
+++ frontend/src/components/home/FoodGallery.tsx
@@ -1,18 +1,44 @@
-import useSearch from 'src/hooks/useSearch'
-import FoodDisplay from './FoodDisplay'
+import useSearch from 'src/hooks/useSearch';
+import FoodDisplaySkeleton from 'src/components/home/FoodDisplaySkeleton';
+import FoodDisplay from 'src/components/home/FoodDisplay';
+import Lottie from 'lottie-react';
+import empty from 'src/assets/empty.json';
+import { Dish } from 'src/types/types';
 
 /**
  * Container for all FoodDisplay components on the main page.
  */
-export default function FoodGallery() {
-  const { data } = useSearch()
-  const { dishes } = data || {}
+type ComponentProps = {
+  isLoading: boolean;
+  dishes: Dish[];
+};
+export default function FoodGallery(props: ComponentProps) {
+  const { isLoading, dishes } = props;
+
+  if (dishes === undefined || dishes.length == 0) {
+    return (
+      <div className='flex w-full h-full items-center justify-center flex-col'>
+        <p className='text-2xl'>No dishes found</p>
+        <Lottie animationData={empty} loop={false} className=' w-1/2' />
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className='flex flex-wrap gap-10'>
+        {Array.from({ length: 10 }).map((_, i) => (
+          <FoodDisplaySkeleton key={i} />
+        ))}
+      </div>
+    );
+  }
 
   return (
-    <div className='flex flex-wrap gap-10'>
+    <div className='flex flex-wrap '>
       {dishes !== undefined &&
         dishes !== null &&
         dishes.map((dish) => <FoodDisplay key={dish.dishId} dish={dish} />)}
     </div>
-  )
+  );
 }
