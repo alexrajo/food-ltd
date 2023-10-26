86bc131a7847e34d93adbf369bbb1e3c64a172d7
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-10-26 15:54:55 +0200

    [#54] feat: added bg color with darkmode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
-import { Outlet } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
-    <div className='h-screen select-none overflow-hidden bg-stone-300 text-primary-text'>
-      <Navbar className='row col-span-2' />
-      <div className='flex h-full'>
+    <div className='flex flex-row h-screen light:text-black dark:text-white'>
+      <Navbar />
+      <div className='flex h-full w-full light:bg-lemon dark:bg-primarydark'>
         <Outlet />
       </div>
     </div>
-  )
+  );
 }
