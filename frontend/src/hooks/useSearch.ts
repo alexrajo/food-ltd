93d392bfd372704f54cc9254770e22a4cd0c02b8
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-10-26 15:56:26 +0200

    [#54] feat: modified useSearch to start at 1 in page

--- frontend/src/hooks/useSearch.ts
+++ frontend/src/hooks/useSearch.ts
@@ -1,10 +1,10 @@
-import { SEARCH_TIMEOUT_MS } from 'src/utils/constants'
-import { useQuery } from '@tanstack/react-query'
-import React, { useEffect } from 'react'
-import { fetchSearchResults } from 'src/utils/api-calls'
-import { setKeyWord } from 'src/redux/confinementReducer'
-import { useAppDispatch, useAppSelector } from './useAppRedux'
-import { useSearchReturnType } from './HookTypes'
+import { SEARCH_TIMEOUT_MS } from 'src/utils/constants';
+import { useQuery } from '@tanstack/react-query';
+import React, { useEffect, useState } from 'react';
+import { fetchSearchResults } from 'src/utils/api-calls';
+import { setKeyWord } from 'src/redux/confinementReducer';
+import { useAppDispatch, useAppSelector } from './useAppRedux';
+import { useSearchReturnType } from './HookTypes';
 
 /**
  * Hooks which allows to search for dishes
@@ -22,67 +22,55 @@ import { useSearchReturnType } from './HookTypes'
  */
 function useSearch(): useSearchReturnType {
   /** Raw user input form html element */
-  const [searchInput, setSearchInput] = React.useState<string>('')
+  const [searchInput, setSearchInput] = useState<string>('');
   /** Page number to allow pagination */
-  const [page, setPage] = React.useState<number>(1)
+  const [page, setPage] = useState<number>(1);
 
   /** Grab the confinements from redux store */
-  const { includingFilters, excludingFilters, keyWord, sortingPreference } =
-    useAppSelector((state) => state.confinements)
+  const { includingFilters, excludingFilters, keyWord, sortingPreference } = useAppSelector(
+    (state) => state.confinements
+  );
 
   /** Allows to modify the redux store */
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
   /** Fetch the data from the api */
   const { isLoading, error, data, refetch } = useQuery({
-    queryKey: [
-      'searchResults',
-      keyWord,
-      page,
-      includingFilters,
-      excludingFilters,
-    ],
+    queryKey: ['searchResults', keyWord, page, includingFilters, excludingFilters],
     queryFn: () =>
-      fetchSearchResults(
-        includingFilters,
-        excludingFilters,
-        sortingPreference,
-        keyWord,
-        page,
-      ),
+      fetchSearchResults(includingFilters, excludingFilters, sortingPreference, keyWord, page),
     keepPreviousData: true,
-  })
+  });
 
-  useEffect(() => {
-    /** Set a timeout to avoid calling the api too often */
-    const timeout = setTimeout(() => {
-      dispatch(setKeyWord(searchInput))
-    }, SEARCH_TIMEOUT_MS)
-    /** Clear the timeout if the user types too fast */
-    return () => clearTimeout(timeout)
-  }, [searchInput])
+  // useEffect(() => {
+  //   /** Set a timeout to avoid calling the api too often */
+  //   const timeout = setTimeout(() => {
+  //     dispatch(setKeyWord(searchInput));
+  //   }, SEARCH_TIMEOUT_MS);
+  //   /** Clear the timeout if the user types too fast */
+  //   return () => clearTimeout(timeout);
+  // }, [searchInput]);
 
   /** Reset the page number whenever confinements changes */
   useEffect(() => {
-    setPage(0)
-  }, [includingFilters, excludingFilters, keyWord, sortingPreference])
+    setPage(1);
+  }, [includingFilters, excludingFilters, keyWord, sortingPreference]);
 
   /**
    * This function refetches the searchresults based
    * on the current confinements
    */
   const onSearch = () => {
-    refetch()
-  }
+    dispatch(setKeyWord(searchInput));
+    refetch();
+  };
 
-  const onChangeSearchInput = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ): void => {
+  const onChangeSearchInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
     /** Destruct */
-    const { value } = event.target
+    const { value } = event.target;
     /** Update the state */
-    setSearchInput(value)
-  }
+    setSearchInput(value);
+  };
 
   /**
    * Calling this function will increment the page number
@@ -90,29 +78,29 @@ function useSearch(): useSearchReturnType {
    */
   const paginateForwards = () => {
     if (data?.dishes.length === 0) {
-      return
+      return;
     }
-    setPage((prev) => prev + 1)
-  }
+    setPage((prev) => prev + 1);
+  };
 
   /**
    * Calling this function will increment the page number
    * and refetches the data
    */
   const paginateBackwards = () => {
-    if (page === 0) {
-      return
+    if (page === 1) {
+      return;
     }
-    setPage((prev) => prev - 1)
-  }
+    setPage((prev) => prev - 1);
+  };
 
   /**
    * Calling this function will paginate to a
    * specific number
    */
   const paginateTo = (page: number) => {
-    setPage(page)
-  }
+    setPage(page);
+  };
 
   return {
     searchInput,
@@ -124,7 +112,8 @@ function useSearch(): useSearchReturnType {
     paginateForwards,
     paginateBackwards,
     onSearch,
-  }
+    page,
+  };
 }
 
-export default useSearch
+export default useSearch;
