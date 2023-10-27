75c952fbfacda7d58c6dc80af457c53e88c05eac
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-10-27 23:35:45 +0200

    [#54] feat: added manual approach to toggle darkmode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import { useAppSelector } from 'src/hooks/useAppRedux'
 
 export default function App() {
+  const colorMode = useAppSelector((state) => state.theme.value)
+
   return (
-    <div className='light:text-black flex h-screen flex-row dark:text-white'>
-      <Navbar />
-      <div className='light:bg-primary flex h-full w-full dark:bg-primarydark'>
-        <Outlet />
+    <div className={`${colorMode == 'dark' ? 'dark' : ''}`}>
+      <div
+        className={`light:text-black flex h-screen flex-row dark:text-white`}
+      >
+        <Navbar />
+        <div className='light:bg-primary flex h-full w-full dark:bg-primarydark'>
+          <Outlet />
+        </div>
       </div>
     </div>
   )
