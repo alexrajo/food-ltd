6df2586155daef514f64fa65762e8284a1a38cbd
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-10-27 09:32:03 +0200

    [#54] feat: added new routes for favorites and history

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistor, store } from 'src/redux/store'
-import App from 'src/App'
-import 'src/index.css'
-import Home from 'src/pages/Home'
-import DishPage from './pages/Dish'
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from 'src/redux/store';
+import App from 'src/App';
+import 'src/index.css';
+import Home from 'src/pages/Home';
+import DishPage from './pages/Dish';
+import Favorites from 'src/pages/Favorites';
 
 const router = createBrowserRouter([
   {
@@ -29,17 +30,21 @@ const router = createBrowserRouter([
       },
       {
         path: '/favorites',
-        element: <>Favorites</>,
+        element: <Favorites />,
+      },
+      {
+        path: '/search',
+        element: <>Settings</>,
       },
       {
         path: '/settings',
         element: <>Settings</>,
-      }
+      },
     ],
   },
-])
+]);
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -50,5 +55,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         </QueryClientProvider>
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
