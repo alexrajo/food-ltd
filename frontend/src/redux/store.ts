3819e1cdfe5e88bdcb28b547f29d4732ffcb6bdc
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-10-27 09:30:40 +0200

    [#54] feat: updated store to support three new reducers

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,43 +1,48 @@
 // Redux toolkit
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 // Reducers
-import themeReducer from 'src/redux/themeReducer'
+import themeReducer from 'src/redux/themeReducer';
 
 // Redux persist
-import storage from 'redux-persist/lib/storage'
-import sessionStorage from 'redux-persist/lib/storage/session'
+import storage from 'redux-persist/lib/storage';
+import sessionStorage from 'redux-persist/lib/storage/session';
 
-import { persistReducer, persistStore } from 'redux-persist'
-import confinementReducer from './confinementReducer'
-import temperatureUnitReducer from './temperatureUnitReducer'
+import { persistReducer, persistStore } from 'redux-persist';
+import confinementReducer from './confinementReducer';
+import temperatureUnitReducer from './temperatureUnitReducer';
+import searchHistoryReducer from 'src/redux/searchHistoryReducer';
+import historyReducer from 'src/redux/historyReducer';
+import favoritesReducer from 'src/redux/favoritesReducer';
 
 const rootConfig = {
   key: 'root',
   storage,
-  whitelist: ['theme', 'temperatureUnit'],
+  whitelist: ['theme', 'temperatureUnit', 'history', 'searchHistory', 'favorites'],
   blacklist: ['confinements'],
-}
+};
 
 const confinementConfig = {
   key: 'favorites',
   storage: sessionStorage,
-}
+};
 
 const rootReducer = combineReducers({
   theme: themeReducer,
   confinements: persistReducer(confinementConfig, confinementReducer),
   temperatureUnit: temperatureUnitReducer,
-})
+  searchHistory: searchHistoryReducer,
+  history: historyReducer,
+  favorites: favoritesReducer,
+});
 
-const persistedReducer = persistReducer(rootConfig, rootReducer)
+const persistedReducer = persistReducer(rootConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }),
-})
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+});
 
-export const persistor = persistStore(store)
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
