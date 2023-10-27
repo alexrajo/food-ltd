d25d9422b3fc4d5bb027b121886762c850cfefd5
Sondre Pedersen <sondrp@stud.ntnu.no>
2023-10-27 06:54:15 +0200

    [#54] fix: darkmode/lightmode support

--- frontend/src/components/home/SearchSettingsSquare.tsx
+++ frontend/src/components/home/SearchSettingsSquare.tsx
@@ -12,14 +12,14 @@ export default function SearchSettingsSquare() {
       className='md group flex h-14 w-14 flex-col items-center justify-center gap-2 rounded border-2 border-black bg-white p-1 px-3 dark:border-tertiarydark dark:bg-secondarydark '
     >
       {/* The rest of this is not relevant, only for styling */}
-      <div className='relative w-full border border-white'>
-        <div className='absolute -top-1 left-1 h-2 w-2 rounded-full border-2 border-white bg-secondarydark transition-all duration-500 group-hover:left-4'></div>
+      <div className='relative w-full border border-black dark:border-white'>
+        <div className='absolute -top-1 left-1 h-2 w-2 rounded-full border-2 bg-white dark:bg-secondarydark transition-all duration-500 group-hover:left-4'></div>
       </div>
-      <div className='relative w-full border border-white'>
-        <div className='absolute -top-1 left-4 h-2 w-2 rounded-full border-2 border-white bg-secondarydark transition-all duration-500 group-hover:left-1'></div>
+      <div className='relative w-full border border-black dark:border-white'>
+        <div className='absolute -top-1 left-4 h-2 w-2 rounded-full border-2 bg-white dark:bg-secondarydark transition-all duration-500 group-hover:left-1'></div>
       </div>
-      <div className='relative w-full border border-white'>
-        <div className='absolute -top-1 left-1 h-2 w-2 rounded-full border-2 border-white bg-secondarydark transition-all duration-500 group-hover:left-4'></div>
+      <div className='relative w-full border border-black dark:border-white'>
+        <div className='absolute -top-1 left-1 h-2 w-2 rounded-full border-2 bg-white dark:bg-secondarydark transition-all duration-500 group-hover:left-4'></div>
       </div>
     </button>
   )
