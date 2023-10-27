7c1c89036669f708981d220f85982cf93565e41f
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-10-27 17:12:20 +0200

    [#54] feat: decided upon primary and secondary color and changed config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
-import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
-    <div className='flex flex-row h-screen light:text-black dark:text-white'>
+    <div className='light:text-black flex h-screen flex-row dark:text-white'>
       <Navbar />
-      <div className='flex h-full w-full light:bg-lemon dark:bg-primarydark'>
+      <div className='light:bg-primary flex h-full w-full dark:bg-primarydark'>
         <Outlet />
       </div>
     </div>
-  );
+  )
 }
--- frontend/src/components/home/FoodDisplaySkeleton.tsx
+++ frontend/src/components/home/FoodDisplaySkeleton.tsx
@@ -1,12 +1,12 @@
 export default function FoodDisplaySkeleton() {
   return (
-    <div className='group animate-pulse flex w-60 cursor-pointer flex-col light:bg-white dark:bg-tigereye shadow-xl'>
-      <div className='flex rounded-sm bg-secondarydark h-40 p-4 items-center justify-center'>
-        <div className=' rounded-sm h-full w-full aspect-square animate-pulse bg-tertiarydark' />
+    <div className='light:bg-white group flex w-60 animate-pulse cursor-pointer flex-col shadow-xl dark:bg-secondary'>
+      <div className='flex h-40 items-center justify-center rounded-sm bg-secondarydark p-4'>
+        <div className=' aspect-square h-full w-full animate-pulse rounded-sm bg-tertiarydark' />
       </div>
       <div className='p-4 font-bold text-black group-hover:underline'>
-        <div className=' w-3/4 h-2 animate-pulse bg-tertiarydark rounded-full' />
+        <div className=' h-2 w-3/4 animate-pulse rounded-full bg-tertiarydark' />
       </div>
     </div>
-  );
+  )
 }
