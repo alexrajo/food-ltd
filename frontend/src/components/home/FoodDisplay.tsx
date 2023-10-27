2793c2f11aba551d5cb348e9b74985cc254f4075
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-10-27 09:29:11 +0200

    [#54] feat: added animation to fooddisplay

--- frontend/src/components/home/FoodDisplay.tsx
+++ frontend/src/components/home/FoodDisplay.tsx
@@ -1,25 +1,33 @@
-import stockFood from 'src/assets/mockFoodImage.jpg'
-import { Link } from 'react-router-dom'
-import type { Dish } from 'src/types/types'
+import stockFood from 'src/assets/mockFoodImage.jpg';
+import { Link } from 'react-router-dom';
+import type { Dish } from 'src/types/types';
+import pin from 'src/assets/thumbtack.png';
 
 type FoodDisplayProps = {
-  dish: Dish
-}
+  dish: Dish;
+};
 
 /**
- * Used on the main page, to show every dish returned after search, filter etc. Each links to 
+ * Used on the main page, to show every dish returned after search, filter etc. Each links to
  * the individual page for that dish.
  */
 export default function FoodDisplay(props: FoodDisplayProps) {
-  const { dish } = props
-  const { title, dishId: id } = dish
+  const { dish } = props;
+  const { title, dishId: id, imageName } = dish;
+
   return (
-    <Link
-      to={`/dish/${id}`}
-      className='group flex w-60 cursor-pointer flex-col  gap-4 bg-white p-4 shadow-xl'
-    >
-      <img alt='food' className='rounded-md' src={stockFood} />
-      <div className='p-4 font-bold group-hover:underline'>{title}</div>
+    <Link to={`/dish/${id}`} className=' basis-1/1 sm:basis-1/2 lg:basis-1/3 p-2'>
+      <div className=' relative group flex cursor-pointer flex-col light:bg-white dark:bg-tigereye shadow-xl hover:scale-105 transition-all ease-out '>
+        <img
+          alt='food'
+          className='rounded-sm object-cover'
+          src={`http://it2810-43.idi.ntnu.no/images/${imageName}.jpg`}
+        />
+
+        <div className='p-4 font-bold text-black  '>
+          <p className=' line-clamp-1 text-xs lg:text-sm'>{title}</p>
+        </div>
+      </div>
     </Link>
-  )
+  );
 }
