2e8a32123a785e8a9e0be2c93ab14b6fce6f5ffa
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-11-01 16:35:02 +0100

    [#59] feat: used new components and hooks to simplify component

--- frontend/src/components/sidebar/FilterMenu.tsx
+++ frontend/src/components/sidebar/FilterMenu.tsx
@@ -1,27 +1,11 @@
-import menu from 'src/assets/burgermenu.svg'
 import x from 'src/assets/x.svg'
-import add from 'src/assets/add-plus-white.svg'
-import meat from 'src/assets/meat.svg'
-import { useMemo, useState } from 'react'
 import { useAppDispatch, useAppSelector } from 'src/hooks/useAppRedux'
-import { closeFilterMenu, setClosed } from 'src/redux/modalsReducer'
-import {
-  excludeFilter,
-  includeFilter,
-  removeExcludedFilter,
-  removeIncludedFilter,
-} from 'src/redux/confinementReducer'
-import { INGREDIENTS, colors } from 'src/utils/constants'
-import Autosuggest from 'react-autosuggest'
-import { ReactSearchAutocomplete } from 'react-search-autocomplete'
-import {
-  FetchIngredientFilterCountsResponse,
-  fetchIngredientFilterCounts,
-} from 'src/utils/api-calls'
-import useSearch from 'src/hooks/useSearch'
-import { useQuery } from '@tanstack/react-query'
-import searchIcon from 'src/assets/searchIcon.svg'
-import Select from 'react-select'
+import { closeFilterMenu } from 'src/redux/modalsReducer'
+import { excludeFilter, includeFilter } from 'src/redux/confinementReducer'
+import AutocompleteIngredients from 'src/components/sidebar/AutocompleteIngredients'
+import IngredientsList from 'src/components/sidebar/IngredientsList'
+import useAutocomplete from 'src/hooks/useAutocomplete'
+import useFilter from 'src/hooks/useFilter'
 
 /**
  * For holding the different filters on the right side
@@ -30,71 +14,19 @@ import Select from 'react-select'
 export default function FilterMenu() {
   const open = useAppSelector((state) => state.modals.filterMenu)
 
-  const menuClass = open
-    ? 'translate-y-0 md:translate-x-0'
-    : 'translate-y-full md:translate-x-full'
+  const menuClass = open ? 'flex' : 'hidden'
 
-  const colorMode = useAppSelector((state) => state.theme.value)
+  const { isLoading, parsedExcludedIngredients, parsedIncludedIngredients } =
+    useAutocomplete()
 
-  const searchInput = useAppSelector((state) => state.confinements.keyWord)
-
-  const includedIngredients = useAppSelector(
-    (state) => state.confinements.includedIngredients,
-  )
-  const excludedIngredients = useAppSelector(
-    (state) => state.confinements.excludedIngredients,
-  )
-
-  /** Fetch the data from the api */
-  const {
-    isLoading,
-    error,
-    data: ingredientCount,
-  } = useQuery({
-    queryKey: [
-      'ingredientCounts',
-      searchInput,
-      includedIngredients,
-      excludedIngredients,
-    ],
-    queryFn: () =>
-      fetchIngredientFilterCounts(
-        searchInput,
-        excludedIngredients,
-        includedIngredients,
-        INGREDIENTS.map((i) => i.name)
-          .filter((i) => !excludedIngredients.map((i) => i.name).includes(i))
-          .filter((i) => !includedIngredients.map((i) => i.name).includes(i)),
-      ),
-  })
-
-  const {
-    includedIngredients: includedIngredientsMap,
-    excludedIngredients: excludedIngredientsMap,
-  } =
-    ingredientCount && ingredientCount.ingredientFilterCounts
-      ? ingredientCount.ingredientFilterCounts.data
-      : {}
-
-  const parsedIncludedIngredients = includedIngredientsMap
-    ? (JSON.parse(includedIngredientsMap) as {
-        [key: string]: number
-      })
-    : {}
-
-  const parsedExcludedIngredients = excludedIngredientsMap
-    ? (JSON.parse(excludedIngredientsMap) as {
-        [key: string]: number
-      })
-    : {}
+  const { includedIngredients, excludedIngredients } = useFilter()
 
   const dispatch = useAppDispatch()
 
   return (
     <div
-      className={`absolute right-0 z-50 flex h-full overflow-y-scroll md:h-full ${menuClass} w-full flex-col gap-6 bg-white px-10 py-20 shadow-xl transition-all dark:bg-secondarydark md:w-96`}
+      className={`absolute right-0 z-50 flex h-3/5 overflow-y-scroll md:h-full ${menuClass} w-full flex-col gap-6 bg-white px-10 py-20 shadow-xl transition-all dark:bg-secondarydark md:w-96`}
     >
-      {/* Menu with profile. I assume we are not doing profile? In that case delete name*/}
       <div className='flex gap-2'>
         <img
           className='h-10 cursor-pointer'
@@ -106,161 +38,24 @@ export default function FilterMenu() {
           }}
         />
       </div>
-
       <div className='flex flex-col gap-2'>
         <div className='text-start text-xl font-bold'>Include ingredients</div>
-        <Select
-          options={
-            isLoading
-              ? []
-              : Object.keys(parsedIncludedIngredients).map((key, index) => ({
-                  name: key,
-                  id: index,
-                  count: parsedIncludedIngredients[key],
-                }))
-          }
-          getOptionLabel={(option) =>
-            `${option.name} ${
-              isLoading
-                ? ''
-                : '(' + parsedIncludedIngredients[option.name] + ')'
-            }`
-          }
-          getOptionValue={(option) => option.name}
+        <AutocompleteIngredients
           isLoading={isLoading}
+          parsedIncludedIngredients={parsedIncludedIngredients}
           onChange={(e) => dispatch(includeFilter(e))}
-          escapeClearsValue
-          value={null}
-          placeholder='Search for ingredients'
-          autoFocus
-          styles={{
-            menu: (provided) => ({
-              ...provided,
-              zIndex: 9999,
-              backgroundColor: 'black',
-            }),
-            menuList: (provided) => ({
-              ...provided,
-              zIndex: 9999,
-              backgroundColor:
-                colorMode == 'dark' ? colors.tertiarydark : 'white',
-            }),
-            option: (provided, state) => ({
-              ...provided,
-              color: state.isSelected ? 'white' : 'black',
-              backgroundColor: state.isSelected
-                ? colors.tertiarydark
-                : 'transparent',
-              '&:hover': {
-                backgroundColor: colors.tertiarydark,
-                color: 'white',
-              },
-            }),
-          }}
-          className='z-999 '
         />
-
-        <div className='flex flex-wrap gap-4'>
-          {includedIngredients.map((ingredient) => (
-            <IncludingFilterBox
-              name={ingredient.name}
-              count={parsedIncludedIngredients[ingredient.name] || '0'}
-            />
-          ))}
-        </div>
+        <IngredientsList ingredients={includedIngredients} />
       </div>
       <div className='flex flex-col gap-2'>
         <div className='text-start text-xl font-bold'>Exclude ingredients</div>
-        <Select
-          options={
-            isLoading
-              ? []
-              : Object.keys(parsedExcludedIngredients).map((key, index) => ({
-                  name: key,
-                  id: index,
-                  count: parsedExcludedIngredients[key],
-                }))
-          }
-          getOptionLabel={(option) =>
-            `${option.name} (${
-              isLoading ? '' : parsedExcludedIngredients[option.name]
-            })`
-          }
-          value={null}
-          escapeClearsValue
-          getOptionValue={(option) => option.name}
+        <AutocompleteIngredients
           isLoading={isLoading}
+          parsedIncludedIngredients={parsedExcludedIngredients}
           onChange={(e) => dispatch(excludeFilter(e))}
-          autoFocus
-          placeholder='Search for ingredients'
-          defaultValue={null}
-          className='z-999'
-          styles={{
-            menu: (provided) => ({
-              ...provided,
-              zIndex: 9999,
-              backgroundColor: 'black',
-            }),
-            menuList: (provided) => ({
-              ...provided,
-              zIndex: 9999,
-              backgroundColor:
-                colorMode == 'dark' ? colors.tertiarydark : 'white',
-            }),
-            option: (provided, state) => ({
-              ...provided,
-              color: state.isSelected ? 'white' : 'black',
-              backgroundColor: state.isSelected
-                ? colors.tertiarydark
-                : 'transparent',
-              '&:hover': {
-                backgroundColor: colors.tertiarydark,
-                color: 'white',
-              },
-            }),
-          }}
         />
-
-        <div className='flex flex-wrap gap-4'>
-          {excludedIngredients.map((ingredient) => (
-            <ExcludingFilterBox
-              name={ingredient.name}
-              count={parsedExcludedIngredients[ingredient.name] || '0'}
-            />
-          ))}
-        </div>
+        <IngredientsList ingredients={excludedIngredients} />
       </div>
-    </div>
-  )
-}
-
-function IncludingFilterBox(props) {
-  const { name } = props
-
-  return (
-    <div
-      className={`flex flex-col items-center justify-center rounded-md border bg-secondary px-2 `}
-    >
-      <div className='text-sm font-bold '>{name}</div>
-    </div>
-  )
-}
-
-function ExcludingFilterBox(props) {
-  const { name } = props
-  return (
-    <div
-      className={`flex flex-col items-center justify-center rounded-md border bg-secondary px-2 `}
-    >
-      <div className='text-sm font-bold '>{name}</div>
-    </div>
-  )
-}
-
-function FilterBoxAdd() {
-  return (
-    <div className='flex h-16 w-16 items-center justify-center rounded-md border-4 border-secondary'>
-      <img className='h-8' src={add} />
     </div>
   )
 }
