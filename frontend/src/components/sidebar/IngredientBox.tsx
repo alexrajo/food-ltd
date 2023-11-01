3ba8f515d057996ead54abdd037ad075adf2c0d8
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-11-01 16:34:46 +0100

    [#59] feat: made component for ingredientbox

---
+++ frontend/src/components/sidebar/IngredientBox.tsx
@@ -0,0 +1,14 @@
+type ComponentProps = {
+  name: string
+}
+export default function IngredientBox(props: ComponentProps) {
+  const { name } = props
+
+  return (
+    <div
+      className={`flex flex-col items-center justify-center rounded-md border bg-secondary px-2 `}
+    >
+      <div className='text-sm font-bold '>{name}</div>
+    </div>
+  )
+}
