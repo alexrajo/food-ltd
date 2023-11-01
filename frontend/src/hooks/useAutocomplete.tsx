60ab779b9316abc0770284f5b44701147cf46a2d
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-11-01 16:34:19 +0100

    [#59] feat: added hooks to support autocomplete

---
+++ frontend/src/hooks/useAutocomplete.tsx
@@ -0,0 +1,93 @@
+import { useQuery } from '@tanstack/react-query'
+import { useAutoCompleteReturnType } from 'src/hooks/HookTypes'
+import { useAppSelector } from 'src/hooks/useAppRedux'
+import { fetchIngredientFilterCounts } from 'src/utils/api-calls'
+import { INGREDIENTS } from 'src/utils/constants'
+
+/**
+ * Hook for the autocomplete functionality
+ * @example
+ *
+ * const MyComponent = () => {
+ * const { isLoading, parsedIncludedIngredients, parsedExcludedIngredients } = useAutocomplete()
+ *  return (
+ *   <div>
+ *    {isLoading ? (
+ *     <p>Loading...</p>
+ *   ) : (
+ *    <div>
+ *    <p>Included Ingredients</p>
+ *   {Object.keys(parsedIncludedIngredients).map((key) => (
+ *    <p>{key}</p>
+ *  ))}
+ * <p>Excluded Ingredients</p>
+ * {Object.keys(parsedExcludedIngredients).map((key) => (
+ *  <p>{key}</p>
+ * ))}
+ * </div>
+ * ))}
+ * </div>
+ * )
+ * }
+ *
+ * @returns The parsed included and excluded ingredients
+ * @returns Whether the data is loading
+ */
+export default function useAutocomplete(): useAutoCompleteReturnType {
+  /** The search input */
+  const searchInput = useAppSelector((state) => state.confinements.keyWord)
+
+  /** The included and excluded ingredients */
+  const includedIngredients = useAppSelector(
+    (state) => state.confinements.includedIngredients,
+  )
+  const excludedIngredients = useAppSelector(
+    (state) => state.confinements.excludedIngredients,
+  )
+
+  /** Fetch the data from the api */
+  const { isLoading, data: ingredientCount } = useQuery({
+    queryKey: [
+      'ingredientCounts',
+      searchInput,
+      includedIngredients,
+      excludedIngredients,
+    ],
+    queryFn: () =>
+      fetchIngredientFilterCounts(
+        searchInput,
+        excludedIngredients,
+        includedIngredients,
+        INGREDIENTS.map((i) => i.name)
+          .filter((i) => !excludedIngredients.map((i) => i.name).includes(i))
+          .filter((i) => !includedIngredients.map((i) => i.name).includes(i)),
+      ),
+  })
+
+  /** The included and excluded ingredients as a map */
+  const {
+    includedIngredients: includedIngredientsMap,
+    excludedIngredients: excludedIngredientsMap,
+  } =
+    ingredientCount && ingredientCount.ingredientFilterCounts
+      ? ingredientCount.ingredientFilterCounts.data
+      : { includedIngredients: null, excludedIngredients: null }
+
+  /** The parsed included and excluded ingredients */
+  const parsedIncludedIngredients = includedIngredientsMap
+    ? (JSON.parse(includedIngredientsMap) as {
+        [key: string]: number
+      })
+    : {}
+  const parsedExcludedIngredients = excludedIngredientsMap
+    ? (JSON.parse(excludedIngredientsMap) as {
+        [key: string]: number
+      })
+    : {}
+
+  return {
+    isLoading,
+    parsedIncludedIngredients,
+    parsedExcludedIngredients,
+  }
+}
