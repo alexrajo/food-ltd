2b2da35c1419152cadfb39d7f292c312e47c9d93
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-11-01 17:03:19 +0100

    [#60] feat: added functionality for closing the modals when pressing a navbar element

--- frontend/src/components/navbar/NavElement.tsx
+++ frontend/src/components/navbar/NavElement.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useLocation, useParams } from 'react-router-dom'
+import { useAppDispatch } from 'src/hooks/useAppRedux'
+import { closeFilterMenu, closeNavbar } from 'src/redux/modalsReducer'
 
 type ComponentProps = {
   icon: React.ReactNode
@@ -14,9 +16,14 @@ export default function NavElement(props: ComponentProps) {
 
   const selected = link == location.pathname
 
+  const dispatch = useAppDispatch()
   return (
     <Link
       to={link}
+      onClick={() => {
+        dispatch(closeNavbar())
+        dispatch(closeFilterMenu())
+      }}
       className=' relative flex w-72 flex-row items-center gap-2 px-20 '
     >
       {icon}
