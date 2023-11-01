c995223388c9043a56665f9fe5f014f0df6a3e1a
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-11-01 17:04:50 +0100

    [#60] feat: fooddisplay now displayes rating

--- frontend/src/components/home/FoodDisplay.tsx
+++ frontend/src/components/home/FoodDisplay.tsx
@@ -2,6 +2,8 @@ import stockFood from 'src/assets/mockFoodImage.jpg'
 import { Link } from 'react-router-dom'
 import type { Dish } from 'src/types/types'
 import pin from 'src/assets/thumbtack.png'
+import { memo } from 'react'
+import RatingDisplay from 'src/components/RatingDisplay'
 
 type FoodDisplayProps = {
   dish: Dish
@@ -11,15 +13,12 @@ type FoodDisplayProps = {
  * Used on the main page, to show every dish returned after search, filter etc. Each links to
  * the individual page for that dish.
  */
-export default function FoodDisplay(props: FoodDisplayProps) {
+function FoodDisplay(props: FoodDisplayProps) {
   const { dish } = props
-  const { title, dishId: id, imageName } = dish
+  const { title, dishId: id, imageName, averageRating } = dish
 
   return (
-    <Link
-      to={`/dish/${id}`}
-      className=' basis-1/1 p-2 sm:basis-1/2 lg:basis-1/3'
-    >
+    <Link to={`/dish/${id}`} className=' basis-1/2 p-2 lg:basis-1/3'>
       <div className='group relative flex cursor-pointer flex-col bg-white shadow-xl transition-all ease-out hover:scale-105 dark:bg-secondary '>
         <img
           alt='food'
@@ -29,8 +28,14 @@ export default function FoodDisplay(props: FoodDisplayProps) {
 
         <div className='p-4 font-bold text-black  '>
           <p className=' line-clamp-1 text-xs lg:text-sm'>{title}</p>
+          <RatingDisplay
+            rating={averageRating}
+            // rating={undefined}
+          />
         </div>
       </div>
     </Link>
   )
 }
+
+export default memo(FoodDisplay)
