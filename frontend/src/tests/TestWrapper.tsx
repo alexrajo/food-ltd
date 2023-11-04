c66f55e4f8f591d6ec95f29a0e8fec3904a0244d
Sondre Pedersen <sondrp@stud.ntnu.no>
2023-11-04 17:01:43 +0100

    [#76] feat: upgrade old testwrapper

--- frontend/src/tests/TestWrapper.tsx
+++ frontend/src/tests/TestWrapper.tsx
@@ -2,23 +2,58 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Provider } from 'react-redux'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from 'src/App'
+import DishPage from 'src/pages/Dish'
+import ErrorPage from 'src/pages/Error'
+import Favorites from 'src/pages/Favorites'
 import Home from 'src/pages/Home'
+import Settings from 'src/pages/Settings'
+import WriteReview from 'src/pages/WriteReview'
 import { store } from 'src/redux/store'
 
 const queryClient = new QueryClient()
 
-const router = createBrowserRouter([
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/dish/:id',
+          element: <DishPage />,
+        },
+        {
+          path: '/dish/:id/write-review',
+          element: <WriteReview />,
+        },
+        {
+          path: '*',
+          element: <ErrorPage />,
+        },
+        {
+          path: '/favorites',
+          element: <Favorites />,
+        },
+        {
+          path: '/search',
+          element: <>Settings</>,
+        },
+        {
+          path: '/settings',
+          element: <Settings />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-    ],
+    basename: import.meta.env.BASE_URL,
   },
-])
+)
 
 export default function TestWrapper() {
   return (
