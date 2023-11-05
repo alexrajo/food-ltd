b186ac4ccbefc6f7cd1deecc12838458f7582765
Sondre Pedersen <sondrp@stud.ntnu.no>
2023-11-05 14:05:34 +0100

    [#76] test: upgrade food gallery

--- frontend/src/tests/components/home/FoodGallery.test.tsx
+++ frontend/src/tests/components/home/FoodGallery.test.tsx
@@ -1,16 +1,27 @@
-import { describe, test, expect, beforeEach } from 'vitest'
+import { describe, test, expect, beforeEach, vi } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import TestWrapper from 'src/tests/TestWrapper'
 
-describe('FoodGallery', () => {
+// Mock lottie to not run pointless animations in the test
+vi.mock('lottie-react', () => {
+  return {
+    default: ({
+      animationData,
+      loop,
+      className,
+    }: {
+      animationData: string
+      loop: boolean
+      className: string
+    }) => <div>{loop}</div>,
+  }
+})
+
+describe('FilterDisplay', async () => {
   beforeEach(async () => {
-    render(<div />, {
-      wrapper: TestWrapper,
-    })
-    // Wait for the data to arrive before testing
-    await screen.findByText(
-      'Miso-Butter Roast Chicken With Acorn Squash Panzanella',
-    )
+    render(<div />, { wrapper: TestWrapper })
+
+    await screen.findByText('Mock Dish 1')
   })
   test('should load Food display components', () => {
     expect(screen.queryAllByAltText('food').length).toBeGreaterThan(0)
