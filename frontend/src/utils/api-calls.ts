45d7ccceba64a1b4e33b42844c04df478302fd4e
armandyblakstvedt <89578819+armandyblakstvedt@users.noreply.github.com>
2023-11-22 23:16:00 +0100

    fix: uncommented code that should not have been commented

--- frontend/src/utils/api-calls.ts
+++ frontend/src/utils/api-calls.ts
@@ -7,10 +7,9 @@ import mock_ingredients from 'src/tests/mock/ingredients.json'
 import readme from '../../../README.md'
 import { Section, readmeParser } from './readmeParse'
 
-const URL = `http://it2810-43.idi.ntnu.no:4000/graphql`
-// const URL = `http://${
-//   process.env.NODE_ENV === 'production' ? 'it2810-43.idi.ntnu.no' : '127.0.0.1'
-// }:4000/graphql`
+const URL = `http://${
+  process.env.NODE_ENV === 'production' ? 'it2810-43.idi.ntnu.no' : '127.0.0.1'
+}:4000/graphql`
 
 type FetchDishResponse = {
   dish: {
